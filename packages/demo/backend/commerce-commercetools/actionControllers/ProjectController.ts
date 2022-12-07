96607f4c359d0c1fd122739f9cda65549b71c098
Behnam Tehrani <behnam.tehrani@commercetools.com>
2022-12-07 15:19:33 -0500

    Add colors endpoint to project

--- packages/demo/backend/commerce-commercetools/actionControllers/ProjectController.ts
+++ packages/demo/backend/commerce-commercetools/actionControllers/ProjectController.ts
@@ -1,6 +1,7 @@
 import { ActionContext, Request, Response } from '@frontastic/extension-types';
 import { getLocale } from '../utils/Request';
 import { ProjectApi } from '../apis/ProjectApi';
+import { FilterFieldTypes } from '@Types/product/FilterField';
 
 type ActionHook = (request: Request, actionContext: ActionContext) => Promise<Response>;
 
@@ -12,6 +13,31 @@ export const getProjectSettings: ActionHook = async (request: Request, actionCon
   const response: Response = {
     statusCode: 200,
     body: JSON.stringify(project),
+    sessionData: request.sessionData,
+  };
+
+  return response;
+};
+
+export const colors: ActionHook = async (request: Request) => {
+  const colors = [
+    {
+      field: 'bgColor',
+      type: FilterFieldTypes.ENUM,
+      label: 'Background Color',
+      values: ['primary', 'accent', 'secondary', 'neutral', 'white', 'gray'],
+    },
+    {
+      field: 'textColor',
+      type: FilterFieldTypes.ENUM,
+      label: 'Text Color',
+      values: ['primary', 'accent', 'secondary', 'neutral', 'white', 'gray'],
+    },
+  ];
+
+  const response: Response = {
+    statusCode: 200,
+    body: JSON.stringify(colors),
     sessionData: request.sessionData,
   };
 
