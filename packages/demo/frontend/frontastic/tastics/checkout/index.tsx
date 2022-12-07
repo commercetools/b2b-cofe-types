fcc468b4d4f9e47b7f6bf3ea45ae4a8c40572e8b
Kirby Fike <kirbyfike@gmail.com>
2022-12-07 11:50:49 -0800

    one step checkout

--- packages/demo/frontend/frontastic/tastics/checkout/index.tsx
+++ packages/demo/frontend/frontastic/tastics/checkout/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import AdyenCheckout from 'components/commercetools-ui/adyen-checkout';
+import AdyenOneStepCheckout from 'components/commercetools-ui/adyen-one-step-checkout';
 
 const CheckoutTastic = ({ data }) => {
   return (
-    <AdyenCheckout termsLink={data.termsLink} cancellationLink={data.cancellationLink} privacyLink={data.privacyLink} />
+    <AdyenOneStepCheckout termsLink={data.termsLink} cancellationLink={data.cancellationLink} privacyLink={data.privacyLink} />
   );
 };
 
